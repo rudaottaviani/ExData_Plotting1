5def0b9ddd70667e24c702f4fea999b29af7a829
Ruda Ottaviani <ruda.ottaviani@gmail.com>
2015-07-07 17:47:21 +0200

    plot1 Done!

--- plot1.R
+++ plot1.R
@@ -1,1 +1,10 @@
-source("loadData.R")
+#source("loadData.R")
+
+png(filename="plot1.png", width=480, height=480)
+
+hist(data$Global_active_power,
+     xlab="Global Active Power (Kilowatts)",
+     main="Global Active Power",
+     col="red")
+
+dev.off()
